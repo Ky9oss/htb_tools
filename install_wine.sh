10e890df67f6b6f93cec2bc3ec70b65b5f367396
Ky9oss <104215157+Ky9oss@users.noreply.github.com>
2025-09-03 20:03:16 +0800

    Delete install_wine.sh

--- install_wine.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get -y install win64
-sudo dpkg --add-architecture i386 && sudo apt-get -y update 
-sudo apt-get -y install wine32:i386
-sudo apt install winetricks -y
-# winetricks dotnet40
